02a6b75974faed49072b77730414792f3eefae41
yudistiraardi <mail.ardix@gmail.com>
2023-09-09 19:16:29 +0700

    First commit

---
+++ appSidebar.js
@@ -0,0 +1,25 @@
+
+    // SIDEBAR
+    
+const btn = document.querySelector('.sidebar-toggle');
+const btnCls = document.querySelector('.fa');
+const sidebar = document.querySelector('.sidebar');
+
+    btn.addEventListener('click', function() {
+
+        if (sidebar.classList.contains('show-sidebar')) {
+
+            sidebar.classList.remove('show-sidebar');
+
+        } else {
+
+            sidebar.classList.add('show-sidebar');
+        };
+
+    })
+
+    btnCls.addEventListener('click', function() {
+
+        sidebar.classList.remove('show-sidebar');
+
+    })
