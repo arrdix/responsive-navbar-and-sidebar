051f29ec8bddf4be583a3e0f8099cd3ac01597c8
yudistiraardi <mail.ardix@gmail.com>
2023-09-11 10:14:43 +0700

    Update codes

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-    // METHOD
-
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
-
-    // MANUAL TOGGLE
+// MANUAL TOGGLE
 
 //    if (links.classList.contains('show-links')) {
 //     links.classList.remove('show-links');
@@ -14,41 +6,36 @@
 //     links.classList.add('show-links');
 //    }
 
-    // NAVBAR
-const btn = document.querySelector('.nav-toggle');
-const links = document.querySelector('.links');
-const info = document.querySelectorAll('.fab');
+// NAVBAR
 
-btn.addEventListener('click', function(btns) {
+const btn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+const info = document.querySelectorAll(".fab");
 
-    links.classList.toggle('show-links');
+btn.addEventListener("click", function (btns) {
+  links.classList.toggle("show-links");
 
-    for (let i = 0; i < info.length; i++) {
-
-        if (links.classList.contains('show-links')) {
-        
-            if (info[i].classList.contains('fa-facebook')) {
-                info[i].textContent = ' Facebook';
-            } else if (info[i].classList.contains('fa-twitter')) {
-                info[i].textContent = ' Twitter';
-            } else if (info[i].classList.contains('fa-behance')) {
-                info[i].textContent = ' Behance';
-            } else {
-                info[i].textContent = ' LinkedIn';
-            }
-        
-        } else {
-
-            if (info[i].classList.contains('fa-facebook')) {
-                info[i].textContent = ' ';
-            } else if (info[i].classList.contains('fa-twitter')) {
-                info[i].textContent = ' ';
-            } else if (info[i].classList.contains('fa-behance')) {
-                info[i].textContent = ' ';
-            } else {
-                info[i].textContent = ' ';
-            }
-
-        }
+  for (let i = 0; i < info.length; i++) {
+    if (links.classList.contains("show-links")) {
+      if (info[i].classList.contains("fa-facebook")) {
+        info[i].textContent = " Facebook";
+      } else if (info[i].classList.contains("fa-twitter")) {
+        info[i].textContent = " Twitter";
+      } else if (info[i].classList.contains("fa-behance")) {
+        info[i].textContent = " Behance";
+      } else {
+        info[i].textContent = " LinkedIn";
+      }
+    } else {
+      if (info[i].classList.contains("fa-facebook")) {
+        info[i].textContent = " ";
+      } else if (info[i].classList.contains("fa-twitter")) {
+        info[i].textContent = " ";
+      } else if (info[i].classList.contains("fa-behance")) {
+        info[i].textContent = " ";
+      } else {
+        info[i].textContent = " ";
+      }
     }
+  }
 });
